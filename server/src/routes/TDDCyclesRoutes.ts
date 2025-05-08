ef9fa9cadac12f94e3990b08d0344a4c33cc16c9
Alepm22 <alejandra.palza@ucb.edu.bo>
2025-05-08 07:24:15 -0400

    Guardando cambios locales

--- server/src/routes/TDDCyclesRoutes.ts
+++ server/src/routes/TDDCyclesRoutes.ts
@@ -22,30 +22,4 @@ TDDCyclesRouter.get(
   async (req, res) => await tddCyclesController.getTDDCycles(req, res)
 );
 
-<<<<<<< HEAD
-// Get all test results from a repository in Github
-TDDCyclesRouter.get(
-  "/jobs",
-  async (req, res) => await tddCyclesController.getTestResults(req, res)
-);
-
-TDDCyclesRouter.post(
-  "/upload-log",
-  async (req, res) => await tddCyclesController.uploadTDDLog(req, res)
-);
-
-TDDCyclesRouter.get( //en teoria aqui deberia recibir el sha del commit
-  "/commit-timeline",
-  async (req, res) => await tddCyclesController.getCommitTimeLine(req, res)
-);
-
-//Ruta para obtener los commits de un repositorio de la BD
-TDDCyclesRouter.get(
-  "/get-commits",
-  async (req, res) => await tddCyclesController.getCommits(req, res)
-);
-
 export default TDDCyclesRouter;
-=======
-export default TDDCyclesRouter;
->>>>>>> 8c9662f412d86b657841161462b5f134037ca568
